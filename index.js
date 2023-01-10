d034bc78bb0f4f454165a563e923de7ac2ec7831
Mateen <zahidx297@yahoo.com>
2023-01-10 03:38:12 -0500

    add webpack

---
+++ index.js
@@ -0,0 +1,5 @@
+var moment = require('moment');
+
+console.log("Hello, World!");
+console.log(`The time is: ${moment().format('MMMM  Do YYYY, h:mm:ss a')}`)
+console.log("Another, Message");
--- javascript.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello, World!");
-console.log(`The time is: ${moment().format('MMMM  Do YYYY, h:mm:ss a')}`)
