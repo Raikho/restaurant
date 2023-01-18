c1e157415f066836b194ccacc72ad719f74b5fac
Mateen <zahidx297@yahoo.com>
2023-01-18 00:14:25 -0500

    add remaining home content dynamically

--- src/index.js
+++ src/index.js
@@ -34,12 +34,53 @@ function addContent(tab) {
         content.node.appendChild(element);
 }
 
-// Add home content;
+// Add home content
 tabs.home.content = [];
-let x = document.createElement('div');
+// Title
+let x  = document.createElement('div');
 x.classList.add('card', 'title');
 x.textContent = 'Samosa Hut';
 tabs.home.content.push(x);
+// Description
+x  = document.createElement('div');
+x.classList.add('card');
+x.textContent = 'Samosa Hut has the best Samosas!';
+tabs.home.content.push(x);
+// Hours
+x = document.createElement('div');
+x.classList.add('card');
+let y = document.createElement('div');
+y.classList.add('heading');
+y.textContent = 'Hours';
+x.appendChild(y);
+let hoursList = [
+    'Sunday: 12am - 4pm',
+    'Monday: 10am - 8pm',
+    'Teusday: 10am - 8pm',
+    'Wednesday: 10am - 8pm',
+    'Thursday: 10am - 8pm',
+    'Friday: 10am - 10pm',
+    'Saturday: 10am - 10pm',
+];
+for (let i of hoursList) {
+    x.appendChild(document.createElement('li'));
+    x.lastChild.textContent = i;
+}
+x.child
+tabs.home.content.push(x);
+// Location
+x  = document.createElement('div');
+x.classList.add('card');
+y = document.createElement('div');
+y.classList.add('heading');
+y.textContent = 'Location';
+x.appendChild(y);
+y = document.createElement('div');
+y.textContent = '9000 Central Ave, Manhattan, New York';
+x.appendChild(y);
+tabs.home.content.push(x);
+
+
 
 clearContent();
 addContent(tabs.home);
