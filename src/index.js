3d736cf47e5cb527c71ba0d15bb23f3598e972cc
Mateen <zahidx297@yahoo.com>
2023-01-17 20:21:43 -0500

    create tabs object and add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log("Hello, World! Try #: 5");
+
+const tabs = {
+    home: {id: 'home'},
+    menu: {id: 'menu'},
+    contact: {id: 'contact'},
+};
+
+
+
+for (let [key, tab] of Object.entries(tabs)) {
+    tab.node = document.getElementById(tab.id);
+    tab.node.addEventListener('click', () => {
+        tab.node.dataset.selected = 'true';
+        Object.entries(tabs).forEach(([key, value]) => {
+            if (tab.id != value.id) {
+                value.node.dataset.selected = 'false';
+            }
+        });
+    });
+}
+
+
+const homeNode = document.getElementById('home');
+const menuNode = document.getElementById('menu');
+const contactNode = document.getElementById('contact');
+
+// menuNode.addEventListener('click', () => {
+//     console.log('menu clicked');
+//     homeNode.dataset.selected = "false";
+//     menuNode.dataset.selected = "true";
+//     contactNode.dataset.selected = "false";
+// });
+
+console.log(tabs);
